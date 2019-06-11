745b0ba5c80fb926df0c866ec93a039619dd688c
ryan-byrne <ryanbyrne142@yahoo.com>
2019-06-11 10:19:26 -0400

    allow for simultaneous updates during GUI launch

--- solis.py
+++ solis.py
@@ -6,7 +6,6 @@ class Solis():
         pass
 
     def check_for_SOLIS():
-        print("Checking if SOLIS is running")
         if "AndorSolis.exe" not in (p.name() for p in psutil.process_iter()):
             print("SOLIS is not connected!")
             return 0
