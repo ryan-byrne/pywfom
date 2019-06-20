4a0aea281bcfb3fb98670b40e41b3985adb95600
Ryan Byrne <ryanbyrne142@gmail.com>
2019-06-20 11:36:02 -0400

    add serial test to arduino script

--- arduino.py
+++ arduino.py
@@ -1,8 +1,21 @@
-from serial import Serial
-from serial import serialutil
+import serial
+from time import sleep
 
 class Arduino():
     """docstring for Arduino."""
 
     def __init__(self):
         pass
+
+
+if __name__ == '__main__':
+    ser = serial.Serial(
+        port='COM4',\
+        baudrate=9600,\
+        parity=serial.PARITY_NONE,\
+        stopbits=serial.STOPBITS_ONE,\
+        bytesize=serial.EIGHTBITS,\
+            timeout=0)
+    while True:
+        print(ser.isOpen())
+        sleep(1)
