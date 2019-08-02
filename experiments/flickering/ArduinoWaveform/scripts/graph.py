7a07e2b930722438201719287e7eecc07f88547a
Ryan Byrne <ryanbyrne142@gmail.com>
2019-08-02 17:29:48 -0400

    add ArduinoWaveform test

--- experiments/flickering/ArduinoWaveform/scripts/graph.py
+++ experiments/flickering/ArduinoWaveform/scripts/graph.py
@@ -6,13 +6,14 @@ import matplotlib.pyplot as plt
 if __name__ == '__main__':
     os.chdir("..")
     colors = ["LIME", "GREEN", "BLUE", "RED"]
-    freq = [10, 25, 50]
+    freq = [1/300, 10, 25, 50]
+    freq = [0]
     for c in colors:
         for f in freq:
             with open("data/%s_100p_%shz.csv" % (c, f), "r") as file:
                 df = pd.read_csv(file, names=["TIME", c])
             file.close()
-            plt.plot(df["TIME"], df[c])
+            plt.scatter(df["TIME"], df[c])
             plt.title("%s at %s Hz" % (c, str(f)))
             plt.xlabel("Time")
             plt.ylabel("%s Intensity" % (c))
