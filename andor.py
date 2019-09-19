3df5ca845aa843133552655aee5a7b9876db1021
Ryan Byrne <ryanbyrne142@gmail.com>
2019-09-19 17:27:52 -0400

    improve error handling for binning above 1x1

--- andor.py
+++ andor.py
@@ -15,23 +15,35 @@ class Andor():
         os.chdir("../..")
         self.hndl = self.sdk3.open(0)
         try:
-            self.sdk3.set_bool(self.hndl, "SensorCooling", True)
+            name = self.sdk3.get_string(self.hndl, "CameraName")
+            print("Successsfully connected to: {0}".format(name))
             self.connected = 1
         except ATCoreException:
-            print("Unable to connect to the Camera!")
+            print("Camera not connected!")
             self.connected = 0
 
     def deploy_settings(self, settings):
         s = settings
+        # Checking binning and aspect ratio
+        self.width, self.height = int(s["width"]), int(s["height"])
+        bin = int(s["binning"][0])
+
+        if bin*self.height > 2048 or bin*self.width > 2048:
+            print("Sizing error with AOI Binning and Dimensions")
+            raise ATCoreException
+
         print("Deploying settings to the Camera")
+        # Default Settings for all runs
+        self.sdk3.set_bool(self.hndl, "SensorCooling", True)
+        self.sdk3.set_enum_string(self.hndl, "CycleMode", "Continuous")
+        self.sdk3.set_enum_string(self.hndl, "PixelEncoding", "Mono32")
+        # Run specific settings
+        self.sdk3.strobe_order = s["strobe_order"]
         self.sdk3.set_float(self.hndl, "ExposureTime", float(s["exposure"]))
         self.exposure_time = self.sdk3.get_float(self.hndl, "ExposureTime")
-        self.sdk3.set_enum_string(self.hndl, "PixelEncoding", "Mono32")
         self.sdk3.set_enum_string(self.hndl, "AOIBinning", s["binning"])
-        self.sdk3.strobe_order = s["strobe_order"]
-        self.sdk3.set_enum_string(self.hndl, "CycleMode", "Continuous")
-        self.height = self.sdk3.set_int(self.hndl, "AOIHeight", int(s["height"]))
-        self.width = self.sdk3.set_int(self.hndl, "AOIWidth", int(s["width"]))
+        self.sdk3.set_int(self.hndl, "AOIHeight", self.height)
+        self.sdk3.set_int(self.hndl, "AOIWidth", self.width)
         self.image_size_bytes = self.sdk3.get_int(self.hndl, "ImageSizeBytes")
 
 
@@ -46,11 +58,9 @@ class Andor():
         self.sdk3.close(self.hndl)
         return(buf)
 
-    def acquire(self, num_frm, exp):
+    def acquire(self, num_frm):
         print("Acquiring {0} frames".format(num_frm))
-        self.sdk3.set_float(self.hndl, "ExposureTime", exp)
-        e = self.sdk3.get_float(self.hndl, "ExposureTime")
-        print("Exposure time set to: {0}".format(e))
+        print("Exposure time set to: {0}".format(self.exposure_time))
         num_buf = num_frm
         buf = np.empty((num_frm, self.image_size_bytes), dtype='uint32')
         for i in range(num_buf):
@@ -70,8 +80,20 @@ class Andor():
 
 if __name__ == '__main__':
     camera = Andor()
-    exposure_time = 0.0068
-    frames = camera.acquire(3, exposure_time)
-    for frame in frames:
-        img = Image.frombytes(size=(camera.height, camera.width), data=frame, mode="I")
-        img.show()
+    settings = {
+                    "exposure":0.0068,
+                    "height":1024,
+                    "width":1024,
+                    "binning":"2x2",
+                    "strobe_order":["Red", "Blue"]
+                    }
+    try:
+        while camera.connected > 0:
+            print(camera.exposure_time)
+            frames = camera.acquire(3)
+            for frame in frames:
+                img = Image.frombytes(size=(camera.height, camera.width), data=frame, mode="I")
+                img.show()
+            time.sleep(10000)
+    except ATCoreException as e:
+        print(e)
