32e083da95d55e2dea8244e8d19bdb1d8f379cd1
ryan-byrne <ryanbyrne142@yahoo.com>
2020-05-01 23:11:16 -0400

    correct typo

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open(os.path.join(os.getcwd(), 'README.md'), encoding='utf-8') as f:
 
 setup(
     name = "OpenWFOM",
-    version = "0.0.1b1",
+    version = "0.0.1b2",
     author = "Ryan Byrne",
     author_email = "ryanbyrne142@gmail.com",
     keywords = "wide field optical mapping",
@@ -14,13 +14,13 @@ setup(
     include_package_data=True,
     py_modules=["wfom"],
     install_requires=[
-        "colorama>=0.4.3",
-        "path>=13.0.0",
-        "psutil>=5.7.0",
-        "pyfiglet>=0.8.post1",
-        "pyserial>=3.4",
-        "pywinauto==0.6.3",
-        "termcolor==1.1.0"
+        "colorama>-0.4.3",
+        "path>-13.0.0",
+        "psutil>-5.7.0",
+        "pyfiglet>-0.8.post1",
+        "pyserial>-3.4",
+        "pywinauto>-0.6.3",
+        "termcolor>-1.1.0"
     ],
     long_description=long_description,
     long_description_content_type='text/markdown',
